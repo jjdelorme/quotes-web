98d91e01728c4c7e12c191c6b418d15f0a90b398
Jason De Lorme <delormej@gmail.com>
2023-12-01 15:32:25 -0500

    Adding quote service implementation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { Quote } from './quote';
+import { QuoteService } from './quote-service';
+import { Observable, of, map } from 'rxjs';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatToolbarModule, MatCardModule, MatButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'quotes';
+export class AppComponent implements OnInit {
+  constructor(private readonly quoteService: QuoteService) {}
+
+  quotes!: Observable<Quote[]>;
+  showRandomQuote = false;
+
+  getAllQuotes() {
+    this.quotes = this.quoteService.getAllQuotes();
+    this.showRandomQuote = false;
+  }
+
+  getRandomQuote() {   
+    // Map a single return quote into an array of quotes.
+    this.quotes = this.quoteService.getRandomQuote()
+      .pipe(map((quote) => [quote]));
+    
+    this.showRandomQuote = true;
+  }
+
+  ngOnInit() {
+    this.getAllQuotes();
+  }
 }
---
+++ src/app/quote-service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Quote } from './quote';
+
+const BaseUrl = 'http://localhost:8083';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class QuoteService {
+
+  constructor(private http: HttpClient) { }
+
+  // Get all quotes
+  getAllQuotes() : Observable<Quote[]> {
+    return this.http.get<Quote[]>(BaseUrl + '/quotes');
+  }
+
+  // Get a random quote
+  getRandomQuote() : Observable<Quote> {
+    return this.http.get<Quote>(BaseUrl + '/quotes/random');
+  }
+}

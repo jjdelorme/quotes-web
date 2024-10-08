61c406eb31ad8c187e95ecbc202f40aa5de78522
Jason De Lorme <jasondel@google.com>
2024-10-08 13:20:53 -0400

    Add error message.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { Quote } from './quote';
 import { QuoteService } from './quote-service';
-import { Observable, of, map } from 'rxjs';
+import { Observable, of, map, catchError } from 'rxjs';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
@@ -21,18 +21,24 @@ export class AppComponent implements OnInit {
   quotes?: Observable<Quote[]> = undefined;
   showRandomQuote = false;
   randomPrompt = '';
+  error?: string;
   
   constructor(private readonly quoteService: QuoteService) {}
 
   getAllQuotes() {
-    this.quotes = this.quoteService.getAllQuotes();
+    this.quotes = this.quoteService.getAllQuotes().pipe(
+      catchError(err => {this.error = err.message; return of([]);})
+    )
     this.showRandomQuote = false;
   }
 
   getRandomQuote() {   
     // Map a single return quote into an array of quotes.
     this.quotes = this.quoteService.getRandomQuote(this.randomPrompt)
-      .pipe(map((quote) => [quote]));
+      .pipe(
+        map((quote) => [quote]),
+        catchError(err => {this.error = err.message; return of();})
+      );
     
     this.showRandomQuote = true;
   }
